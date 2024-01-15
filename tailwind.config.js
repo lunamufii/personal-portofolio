d78ec74e84d50e13389a87c04f10194b30b793de
lunamufii <zidnimshtf@gmail.com>
2024-01-15 09:20:33 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#172858',
+        secondary: '#1A93AE',
+        dark: '#0f172a',
+        grey: '#F7F6F6',
+      },
+    },
+  },
+  plugins: [],
+}
+
